a6f2f091e00b2022242021332e8e3b4837ae571a
Kim Taavo <kim.taavo@digdir.no>
2024-10-08 11:24:35 +0200

    refactor + configure vitejs for library

--- lib/index.ts
+++ lib/index.ts
@@ -1,13 +1,19 @@
 import 'css/tailwind.css';
 
-import {MinidCountDown} from 'components/minid-countdown.ts';
-export {MinidCountDown};
-
 import {MinidButton} from 'components/button.component.ts';
 export {MinidButton};
 
 import {MinidDropdown} from 'components/dropdown.component.ts';
 export {MinidDropdown};
+
+import {MinidMenu} from 'components/menu.component.ts';
+export {MinidMenu};
+
+import {MinidMenuItem} from 'components/menu-item.component.ts';
+export {MinidMenuItem};
+
+import {MinidCountDown} from 'components/minid-countdown.ts';
+export {MinidCountDown};
 
 import {MinidPopup} from 'components/popup.component.ts';
 export {MinidPopup};
