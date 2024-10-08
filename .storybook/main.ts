a1be9d9fa3267660bba5661538625973c21f182b
Kim Taavo <kim.taavo@digdir.no>
2024-10-08 19:27:00 +0200

    wip

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,8 @@ import type { StorybookConfig } from '@storybook/web-components-vite';
 
 const config: StorybookConfig = {
   stories: [
-    '../src/stories/**/*.mdx',
-    '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'
   ],
   addons: [
     '@storybook/addon-links',
