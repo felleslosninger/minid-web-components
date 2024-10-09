7d7347aa6910b9834a0c770a0c60261202c9c5dc
Kim Taavo <kim.taavo@digdir.no>
2024-10-09 20:20:59 +0200

    storybook fix

--- src/mixins/tailwind.mixin.ts
+++ src/mixins/tailwind.mixin.ts
@@ -1,24 +1,27 @@
 import { CSSResultArray, LitElement, unsafeCSS } from 'lit';
 import tailwindStyles from '../tailwind.css?inline';
-import styles from '@digdir/designsystemet-css?inline';
-import theme from '@digdir/designsystemet-theme?inline';
+import dsStyles from '@digdir/designsystemet-css?inline';
+import dsTheme from '@digdir/designsystemet-theme?inline';
 
 export const tailwindCssStyles = unsafeCSS(tailwindStyles);
+export const dsCssStyles = unsafeCSS(dsStyles);
+export const dsCssTheme = unsafeCSS(dsTheme);
 
 export const tailwind = <T extends Constructor<LitElement>>(superClass: T) =>
   class extends superClass {
-    static styles = [tailwindCssStyles];
+    static styles = [
+      tailwindStyles,
+      dsStyles,
+      dsTheme
+    ];
   };
 
 type Constructor<T = {}> = new (...args: any[]) => T;
 
-export class TailwindLitElement extends LitElement {
-  static override styles = [tailwindCssStyles];
-}
-
 export class MinidElement extends LitElement {
   static override styles = [
-    unsafeCSS(styles),
-    unsafeCSS(theme),
+    tailwindCssStyles,
+    dsCssStyles,
+    dsCssTheme
   ] as CSSResultArray;
 }
