36bcae310774646429b9f5bc74a299aff3fa82c1
TerjeBallestad <terj.ba@gmail.com>
2024-10-21 17:34:21 +0200

    RM: comment

--- src/components/icon/nav-aksel.icon-library.ts
+++ src/components/icon/nav-aksel.icon-library.ts
@@ -1,8 +1,6 @@
 import type { IconLibrary } from './icon-library.js';
 import { MidIconName } from '../../types/icon-name';
 
-// const baseUrl = new URL('../../../dist', import.meta.url).toString();
-
 const library: IconLibrary<MidIconName> = {
   name: 'nav-aksel',
   resolver: (name) =>
