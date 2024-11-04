fc2c33dbc0dee820af54fd0d6075bc643c3f83c1
Kim Taavo <kim.taavo@digdir.no>
2024-11-04 09:16:59 +0100

    wip

--- demo/src/app-index.ts
+++ demo/src/app-index.ts
@@ -8,11 +8,12 @@ export class AppIndex extends LitElement {
 
   override render() {
     return html`<div>hei hei hei</div>
-    <mid-countdown expiry=${Date.now() + 2 * 1000}></mid-countdown>
+    <mid-countdown expiry=${Date.now() + 60 * 1000}></mid-countdown>
    <hr/>
     <mid-button  @click=${() => {console.log("You clicked?")}}>Klikk meg...</mid-button>
     <hr/>
     <mid-spinner></mid-spinner>
+    <mid-icon name="books-fill"></mid-icon>
     `;
   }
 }
