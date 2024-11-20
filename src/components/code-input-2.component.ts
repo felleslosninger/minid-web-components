229f4d08002295a68a044f8834532b3cbb86733e
TerjeBallestad <terj.ba@gmail.com>
2024-11-20 12:02:27 +0100

    WIP

--- src/components/code-input-2.component.ts
+++ src/components/code-input-2.component.ts
@@ -1,18 +1,79 @@
 import { css, html, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property, queryAll, state } from 'lit/decorators.js';
 import { styled } from 'src/mixins/tailwind.mixin';
 
 const styles = [css``];
 
 @customElement('mid-code-input-2')
 export class MinidCodeInput2 extends styled(LitElement, styles) {
+  private values = Array<string>();
+
+  @queryAll('input')
+  inputElements!: NodeList;
+
+  @property()
+  value = '';
+
   @property({ type: Number })
-  fields = 5;
+  length = 5;
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    this.values = new Array(this.length).fill('');
+  }
+
+  // el.addEventListener('input', (evt) => this.onInput(evt, index));
+  // el.addEventListener('paste', (evt) => this.onPaste(evt));
+  // el.addEventListener('keydown', (evt) => this.onKeydown(evt, index));
+  // el.addEventListener('focusin', () => this.onFocusIn(index));
+  // el.addEventListener('focusout', () => this.onFocusOut(index));
+
+  handleKeydown(index: number) {
+    return (event: KeyboardEvent) => {
+      console.log(index, event);
+      this.values[index] = (event.target as HTMLInputElement).value;
+      this.value = this.values.join('');
+      console.log(this.values, this.value);
+      console.log(this.inputElements);
+
+      // if (event.key === 'Backspace' && index > 0) {
+      //   if ((this.items[index] as HTMLInputElement).value === '') {
+      //     (this.items[index - 1] as HTMLInputElement).value = '';
+      //     (this.items[index - 1] as HTMLInputElement).focus();
+      //   } else {
+      //     (this.items[index] as HTMLInputElement).value = '';
+      //   }
+      // }
+      // this.setCurrentValue();
+      // this.toggleCompleted();
+    };
+  }
+
+  handleInput(index: number) {
+    return (event: InputEvent) => {
+      console.log(index, event);
+    };
+  }
+
+  private input(index: number) {
+    return html`
+      <input
+        id=${'code-input-' + index}
+        class="bg-background-neutral-subtle"
+        min="0"
+        max="9"
+        size="1"
+        maxlength="1"
+        @keydown=${this.handleKeydown(index)}
+        @input=${this.handleInput(index)}
+      />
+    `;
+  }
 
   override render() {
     const fields = [];
-    for (let index = 0; index <= this.fields; index++) {
-      fields.push(html`<input value=${index} />`);
+    for (let index = 0; index < this.length; index++) {
+      fields.push(this.input(index));
     }
     return html` <div class="flex">${fields}</div> `;
   }
--- src/stories/CodeInput2.stories.ts
+++ src/stories/CodeInput2.stories.ts
@@ -1,8 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
 import '../components/code-input-2.component';
+import { ifDefined } from 'lit/directives/if-defined.js';
 
-type CodeInputProps = {};
+type CodeInputProps = {
+  length?: number;
+};
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
@@ -18,7 +21,9 @@ type Story = StoryObj<CodeInputProps>;
 
 export const Main: Story = {
   args: {},
-  render: (args: CodeInputProps) => {
-    return html`<mid-code-input-2></mid-code-input-2>`;
+  render: ({ length: fields }: CodeInputProps) => {
+    return html`<mid-code-input-2
+      fields="${ifDefined(fields)}"
+    ></mid-code-input-2>`;
   },
 };
