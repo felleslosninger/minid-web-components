e18aa7ce699fc78f14d0188c51426a2e72a4ec46
TerjeBallestad <terj.ba@gmail.com>
2025-05-30 13:30:53 +0200

    update u-datalist demo

--- src/stories/UDatalist.stories.ts
+++ src/stories/UDatalist.stories.ts
@@ -34,12 +34,12 @@ type Story = StoryObj<ComboboxProps>;
 
 export const Main: Story = {
   args: {
-    open: true,
+    open: false,
     stayopenonselect: true,
   },
   decorators: [(story) => html`<div class="mb-64">${story()}</div>`],
   render: ({ open, stayopenonselect }: ComboboxProps) => html`
-    <label for="my-input"> Choose flavor of ice cream </label>
+    <label class="mb-2 block" for="my-input"> Choose a country </label>
     <mid-dropdown
       class="flex"
       ?open=${open}
@@ -47,35 +47,41 @@ export const Main: Story = {
       sync="width"
     >
       <input
-        type="search"
-        class="border"
+        type="tel"
+        class="mid-input"
         slot="trigger"
-        id="my-input"
-        list="my-list"
+        @focus=${(event) => {
+          document.getElementById('my-input')?.focus();
+        }}
       />
-
-      <u-datalist
-        class="max-h-40 overflow-y-auto border"
-        id="my-list"
-        data-sr-singular="%d land"
-        data-sr-plural="%d land"
-      >
-        ${getCountries()
-          .sort((a, b) => a.localeCompare(b))
-          .map((country) => {
-            return html`<u-option
-              class="w-full truncate p-2"
-              value="${country}"
-            >
-              <mid-icon
-                class="h-4 w-6 rounded"
-                library="country"
-                name="${country}"
-              ></mid-icon>
-              <span>${countryLabelsNO[country]}</span></u-option
-            >`;
-          })}
-      </u-datalist>
+      <div class="border">
+        <input type="search" class="w-full" id="my-input" list="my-list" />
+        <u-datalist
+          class="max-h-40 overflow-y-auto"
+          id="my-list"
+          data-sr-singular="%d land"
+          data-sr-plural="%d land"
+        >
+          ${getCountries()
+            .sort((a, b) => a.localeCompare(b))
+            .map((country) => {
+              return html`<u-option
+                class="truncate p-2"
+                @change=${(event) => {
+                  console.log(event);
+                }}
+                value="${country}"
+              >
+                <mid-icon
+                  class="h-4 w-6 overflow-hidden rounded"
+                  library="country"
+                  name="${country}"
+                ></mid-icon>
+                <span>${countryLabelsNO[country]}</span>
+              </u-option>`;
+            })}
+        </u-datalist>
+      </div>
     </mid-dropdown>
 
     <style>
